61288ee6e97aae574255e73adcd28c2bc5f46799
Amin Abdillah <andiabdillah255@gmail.com>
2025-05-26 20:36:10 +0700

    feat: create login page layout and integrate Shadcn UI button

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+const LoginPage = () => {
+  return (
+    <div className="bg-gray-100 h-screen w-screen">
+      <div className="">
+        LoginPage
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
